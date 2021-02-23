c67c7846be2d3cc77a6db52376ce56913cd3ccef
frankx13 <frankx13580@gmail.com>
2021-02-23 19:42:35 +0100

    - Created the menu basic UI + color theme - Implemented the basic navigation through the menu buttons with the View.OnClickListener interface

---
+++ app/src/main/java/com/studio/neopanda/braintastic/PreloadScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.studio.neopanda.braintastic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PreloadScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preload_screen)
+    }
+}
